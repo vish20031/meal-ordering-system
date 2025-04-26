0df6613116946c93edc703c198df7d5fd1f13c86
vish20031 <vishu.chathu.2003@gmail.com>
2025-04-27 00:34:06 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $role = $_POST['role'];
+
+    $users = json_decode(file_get_contents('data/users.json'), true);
+
+    foreach ($users as $user) {
+        if ($user['username'] == $username && $user['password'] == $password && $user['role'] == $role) {
+            $_SESSION['username'] = $username;
+            $_SESSION['role'] = $role;
+
+            if ($role == 'admin') {
+                header('Location: public/admin_page.php');
+            } elseif ($role == 'canteen') {
+                header('Location: public/canteen_page.php');
+            } elseif ($role == 'user') {
+                header('Location: public/user_page.php');
+            }
+            exit();
+        }
+    }
+    echo "<script>alert('Invalid Credentials');window.location.href='public/index.html';</script>";
+}
+?>
+
